06556c1bfed4ae4dacdbb7239e908fb522f3f938
Saravanan Kandaswamy <s4ravanank@gmail.com>
2026-09-13 19:02:12 +0530

    Add Jackson2JsonMessageConverter bean to order and makeline services

--- services/order-service/src/main/java/com/dreamworks/petstore/order/RabbitConfig.java
+++ services/order-service/src/main/java/com/dreamworks/petstore/order/RabbitConfig.java
@@ -1,6 +1,8 @@
 package com.dreamworks.petstore.order;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,5 +21,10 @@ public class RabbitConfig {
     @Bean
     Binding binding(Queue q, DirectExchange e) {
         return BindingBuilder.bind(q).to(e).with("order.created");
+    }
+
+    @Bean
+    MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
